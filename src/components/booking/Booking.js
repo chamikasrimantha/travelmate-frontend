38583c04554bb9f935b970f9bd9a7a661b1888a5
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-07-24 19:45:25 +0530

    booking and half of receipt pages developed

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -1,33 +1,75 @@
 import { Box, Rating, Typography, useMediaQuery } from '@mui/material';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useReactToPrint } from 'react-to-print';
 import Receipt from './Receipt';
+import { useParams } from 'react-router-dom';
+import { getBookingsById } from '../../services/api/booking.service';
+import { getPropertyRatingsByProperty } from '../../services/api/property_rating.service';
 
 export default function Booking() {
-    
-    const booking = {
-        id: 1,
-        userId: 1,
-        propertyId: 1,
-        propertyName: 'Luxury Hotel',
-        propertyLocation: 'Colombo, Sri Lanka',
-        propertyRate: 4.5,
-        propertyReviews: 120,
-        checkinDate: '2024-07-01',
-        checkoutDate: '2024-07-07',
-        totalPrice: 12000,
-        paymentMethod: 'Cash',
-        firstName: 'Chamika',
-        lastName: 'Srimantha',
-        email: 'chamika@gmail.com',
-        address: 'Panadura, LK',
-        phoneno: '+94 76 562 7212',
-        bookingFor: 'Main guest',
-        rentingAdditionals: 'Rent a car',
-        specialRequests: 'No No No No No No No No No No No No No No No No No No No No No No No No',
-        arrivalTime: '2:00 PM - 03:00 PM'
+
+    const { id } = useParams();
+
+    const [booking, setBooking] = useState(null);
+    const [rateData, setRatings] = useState([]);
+    const [averageRating, setAverageRating] = useState(null);
+    const [reviewCount, setReviewCount] = useState(0);
+
+    const fetchBookingById = async () => {
+        const response = await getBookingsById(id);
+        setBooking(response.data);
+        console.log(response);
+    }
+
+    const fetchPropertyRatingsByProperty = async () => {
+        try {
+            const id = booking?.propertyEntity?.id;
+            const ratingsResponse = await getPropertyRatingsByProperty(id);
+            setRatings(ratingsResponse.data);
+
+            // Calculate average rating
+            const totalRating = ratingsResponse.data.reduce((sum, rating) => sum + rating.rate, 0);
+            const avgRating = ratingsResponse.data.length ? (totalRating / ratingsResponse.data.length).toFixed(1) : 0;
+            setAverageRating(avgRating);
+            setReviewCount(ratingsResponse.data.length);
+        } catch (error) {
+            console.error("Error fetching property ratings:", error);
+        }
     };
+
+    useEffect(() => {
+        fetchBookingById();
+    }, []);
+
+    useEffect(() => {
+        if (booking) {
+            fetchPropertyRatingsByProperty();
+        }
+    }, [booking]);
+
+    // const booking = {
+    //     id: 1,
+    //     userId: 1,
+    //     propertyId: 1,
+    //     propertyName: 'Luxury Hotel',
+    //     propertyLocation: 'Colombo, Sri Lanka',
+    //     propertyRate: 4.5,
+    //     propertyReviews: 120,
+    //     checkinDate: '2024-07-01',
+    //     checkoutDate: '2024-07-07',
+    //     totalPrice: 12000,
+    //     paymentMethod: 'Cash',
+    //     firstName: 'Chamika',
+    //     lastName: 'Srimantha',
+    //     email: 'chamika@gmail.com',
+    //     address: 'Panadura, LK',
+    //     phoneno: '+94 76 562 7212',
+    //     bookingFor: 'Main guest',
+    //     rentingAdditionals: 'Rent a car',
+    //     specialRequests: 'No No No No No No No No No No No No No No No No No No No No No No No No',
+    //     arrivalTime: '2:00 PM - 03:00 PM'
+    // };
 
     const getRatingLabel = (rate) => {
         if (rate <= 2) return 'Low';
@@ -65,15 +107,15 @@ export default function Booking() {
                     </Col>
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
                         <div style={squareStyle}>
-                            <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>{booking.propertyName}</h4>
-                            <p>{booking.propertyLocation}</p>
+                            <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>{booking?.propertyEntity?.name}</h4>
+                            <p>{booking?.propertyEntity?.location}</p>
                             <div className="d-flex align-items-center mt-2 mt-md-0">
                                 <Box style={{ backgroundColor: '#184D9D', color: 'white', padding: '5px', borderRadius: '5px', width: '50px', textAlign: 'center' }}>
-                                    {booking.propertyRate}
+                                    {averageRating ? averageRating : 'N/A'}
                                 </Box>
-                                <Rating value={booking.propertyRate} precision={0.1} readOnly style={{ marginLeft: '10px', color: '#FFD700' }} />
+                                <Rating value={Number(averageRating) || 0} precision={0.1} readOnly style={{ marginLeft: '10px', color: '#FFD700' }} />
                                 <Typography variant="body2" style={{ marginLeft: '10px', fontSize: '1rem' }}>
-                                    {getRatingLabel(booking.propertyRate)}
+                                    {averageRating ? getRatingLabel(Number(averageRating)) : 'N/A'}
                                 </Typography>
                             </div>
                         </div>
@@ -81,34 +123,34 @@ export default function Booking() {
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
                         <div style={squareStyle}>
                             <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Guest Details</h4>
-                            <p>Guest Name: {booking.firstName} {booking.lastName}</p>
-                            <p>Email: {booking.email}</p>
-                            <p>Address: {booking.address}</p>
-                            <p>Phone Number: {booking.phoneno}</p>
+                            <p>Guest Name: {booking?.firstName} {booking?.lastName}</p>
+                            <p>Email: {booking?.email}</p>
+                            <p>Address: {booking?.address}</p>
+                            <p>Phone Number: {booking?.phoneNo}</p>
                         </div>
                     </Col>
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
                         <div style={squareStyle}>
                             <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Additional Details</h4>
-                            <p>Booking for: {booking.bookingFor}</p>
-                            <p>Extra services added: {booking.rentingAdditionals}</p>
-                            <p>Special requests: {booking.specialRequests}</p>
-                            <p>Arrival time: {booking.arrivalTime}</p>
+                            <p>Booking for: {booking?.bookingFor}</p>
+                            <p>Extra services added: {booking?.airportShuttle ? 'Airport shuttle added' : ''}, {booking?.rentingCar ? 'Rent a car added' : ''}, {booking?.rentingBicycle ? 'Rent a bicycle added' : ''}</p>
+                            <p>Special requests: {booking?.specialRequests}</p>
+                            <p>Arrival time: {booking?.arrivalTime}</p>
                         </div>
                     </Col>
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
                         <div style={squareStyle}>
                             <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Checkin & Checkout Details</h4>
-                            <p>Check-in Date: {booking.checkinDate}</p>
-                            <p>Check-out Date: {booking.checkoutDate}</p>
-                            <p>Total Length of Stay: {Math.ceil((new Date(booking.checkoutDate) - new Date(booking.checkinDate)) / (1000 * 60 * 60 * 24))} nights</p>
+                            <p>Check-in Date: {booking?.checkinDate}</p>
+                            <p>Check-out Date: {booking?.checkoutDate}</p>
+                            <p>Total Length of Stay: {Math.ceil((new Date(booking?.checkoutDate) - new Date(booking?.checkinDate)) / (1000 * 60 * 60 * 24))} nights</p>
                         </div>
                     </Col>
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
                         <div style={squareStyle}>
                             <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Total price summary</h4>
-                            <p>Payment Method: {booking.paymentMethod}</p>
-                            <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Total: {booking.totalPrice} LKR</h4>
+                            <p>Payment Method: {booking?.paymentMethod}</p>
+                            <h4 style={{ fontWeight: 'bold', fontSize: '1.25rem' }}>Total: {booking?.totalPrice} LKR</h4>
                         </div>
                     </Col>
                     <Col md={6} className="mb-3" style={{ width: isMobile ? '100%' : '70%' }}>
--- src/components/booking/Receipt.js
+++ src/components/booking/Receipt.js
@@ -65,31 +65,31 @@ const Receipt = React.forwardRef((props, ref) => {
             </div>
             <div style={sectionStyle1}>
                 <Typography variant="h6" style={titleStyle}>Property Details</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Name:</strong> {booking.propertyName}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Location:</strong> {booking.propertyLocation}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Rate:</strong> {booking.propertyRate}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Reviews:</strong> {booking.propertyReviews}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Name:</strong> {booking?.propertyEntity?.name}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Location:</strong> {booking?.propertyEntity?.location}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Rate:</strong> {booking?.propertyRate}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Reviews:</strong> {booking?.propertyReviews}</Typography>
             </div>
             <div style={sectionStyle}>
                 <Typography variant="h6" style={titleStyle}>Guest Details</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Name:</strong> {booking.firstName} {booking.lastName}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Email:</strong> {booking.email}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Address:</strong> {booking.address}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Phone Number:</strong> {booking.phoneno}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Name:</strong> {booking?.firstName} {booking?.lastName}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Email:</strong> {booking?.email}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Address:</strong> {booking?.address}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Phone Number:</strong> {booking?.phoneNo}</Typography>
             </div>
             <div style={sectionStyle}>
                 <Typography variant="h6" style={titleStyle}>Booking Details</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Check-in Date:</strong> {booking.checkinDate}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Check-out Date:</strong> {booking.checkoutDate}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Total Price:</strong> {booking.totalPrice} LKR</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Payment Method:</strong> {booking.paymentMethod}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Check-in Date:</strong> {booking?.checkinDate}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Check-out Date:</strong> {booking?.checkoutDate}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Total Price:</strong> {booking?.totalPrice} LKR</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Payment Method:</strong> {booking?.paymentMethod}</Typography>
             </div>
             <div style={sectionStyle2}>
                 <Typography variant="h6" style={titleStyle}>Additional Details</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Booking for:</strong> {booking.bookingFor}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Extra services added:</strong> {booking.rentingAdditionals}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Special requests:</strong> {booking.specialRequests}</Typography>
-                <Typography variant="body1" style={detailStyle}><strong>Arrival time:</strong> {booking.arrivalTime}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Booking for:</strong> {booking?.bookingFor}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Extra services added:</strong> {booking?.rentingAdditionals}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Special requests:</strong> {booking?.specialRequests}</Typography>
+                <Typography variant="body1" style={detailStyle}><strong>Arrival time:</strong> {booking?.arrivalTime}</Typography>
             </div>
         </div>
     );
